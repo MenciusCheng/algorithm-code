c49beec2ee388a3c4b6af2f2ccbaf45137345743
marvelcheng <295567172@qq.com>
2021-12-03 10:40:38 +0800

    feat(code): check in

--- trap/slice/main.go
+++ trap/slice/main.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	subSlice()
+}
+
+func subSlice() {
+	a := []int{0, 1, 2, 3, 4, 5}
+
+	fmt.Println(a[0:1])
+	fmt.Println(a[6:6])
+	fmt.Println(a[5:6])
+}
+
+func sliceItemPointer() {
 	arr := []int{1, 2, 3, 4, 5}
 	res := make([]*int, 0)
 
