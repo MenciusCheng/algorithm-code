b39482762de98af9ccb5fbd3d444d823ef3dd8e3
marvelcheng <295567172@qq.com>
2021-12-13 14:26:48 +0800

    add gen

---
+++ leetcode/gen/gen.go
@@ -0,0 +1,54 @@
+package leetcode
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"text/template"
+)
+
+func Gen(desc, url, cal, month string) error {
+	subject, err := NewSubject(desc, url, cal)
+	if err != nil {
+		panic(err)
+	}
+
+	question := fmt.Sprintf("q%d", subject.QuestionNum)
+
+	tmpl, err := template.New("CheckInSubject").Parse(CheckInSubject)
+	if err != nil {
+		panic(err)
+	}
+
+	fileName := "main.go"
+	directory := fmt.Sprintf("/Users/chengmengwei/goProject/algorithm-code/leetcode/%s/%s", month, question)
+	filePath := fmt.Sprintf("%s%s%s", directory, string(os.PathSeparator), fileName)
+
+	if err := BuildDir(filePath); err != nil {
+		return err
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	err = tmpl.Execute(file, subject)
+	if err != nil {
+		return err
+	}
+
+	cmd, err := exec.Command("gofmt", "-l", "-w", filePath).Output()
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(cmd))
+
+	return nil
+}
+
+// BuildDir 创建目录
+func BuildDir(absDir string) error {
+	return os.MkdirAll(path.Dir(absDir), os.ModePerm) //生成多级目录
+}
