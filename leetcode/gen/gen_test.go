c95dc29da61833c0876c1aeaad9947c599ec6f20
marvelcheng <295567172@qq.com>
2021-12-14 10:55:23 +0800

    check in

--- leetcode/gen/gen_test.go
+++ leetcode/gen/gen_test.go
@@ -4,43 +4,43 @@ import "testing"
 
 func TestGen(t *testing.T) {
 	desc := `
-1. 边界着色
-给你一个大小为 m x n 的整数矩阵 grid ，表示一个网格。另给你三个整数 row、col 和 color 。网格中的每个值表示该位置处的网格块的颜色。
-当两个网格块的颜色相同，而且在四个方向中任意一个方向上相邻时，它们属于同一连通分量。
-连通分量的边界 是指连通分量中的所有与不在分量中的网格块相邻（四个方向上）的所有网格块，或者在网格的边界上（第一行/列或最后一行/列）的所有网格块。
-请你使用指定颜色 color 为所有包含网格块 grid[row][col] 的 连通分量的边界 进行着色，并返回最终的网格 grid 。
+630. 课程表 III
+难度：困难
+这里有 n 门不同的在线课程，按从 1 到 n 编号。给你一个数组 courses ，其中 courses[i] = [durationi, lastDayi] 表示第 i 门课将会 持续 上 durationi 天课，并且必须在不晚于 lastDayi 的时候完成。
+你的学期从第 1 天开始。且不能同时修读两门及两门以上的课程。
+返回你最多可以修读的课程数目。
 
 示例 1：
 
-输入：grid = [[1,1],[1,2]], row = 0, col = 0, color = 3
-输出：[[3,3],[3,2]]
-
+输入：courses = [[100, 200], [200, 1300], [1000, 1250], [2000, 3200]]
+输出：3
+解释：
+这里一共有 4 门课程，但是你最多可以修 3 门：
+首先，修第 1 门课，耗费 100 天，在第 100 天完成，在第 101 天开始下门课。
+第二，修第 3 门课，耗费 1000 天，在第 1100 天完成，在第 1101 天开始下门课程。
+第三，修第 2 门课，耗时 200 天，在第 1300 天完成。
+第 4 门课现在不能修，因为将会在第 3300 天完成它，这已经超出了关闭日期。
 示例 2：
 
-输入：grid = [[1,2,2],[2,3,2]], row = 0, col = 1, color = 3
-输出：[[1,3,3],[2,3,3]]
-
+输入：courses = [[1,2]]
+输出：1
 示例 3：
 
-输入：grid = [[1,1,1],[1,1,1],[1,1,1]], row = 1, col = 1, color = 2
-输出：[[2,2,2],[2,1,2],[2,2,2]]
+输入：courses = [[3,2],[4,3]]
+输出：0
 
-提示：
+提示:
 
-m == grid.length
-n == grid[i].length
-1 <= m, n <= 50
-1 <= grid[i][j], color <= 1000
-0 <= row < m
-0 <= col < n
+1 <= courses.length <= 104
+1 <= durationi, lastDayi <= 104
 `
 
 	url := `
-https://leetcode-cn.com/problems/coloring-a-border/
+https://leetcode-cn.com/problems/course-schedule-iii/
 `
 
 	cal := `
-func colorBorder(grid [][]int, row int, col int, color int) [][]int {
+func scheduleCourse(courses [][]int) int {
 
 }
 `
