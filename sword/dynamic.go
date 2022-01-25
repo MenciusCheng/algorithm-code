f27051f79fedc5b885dba7919decfba90df784f0
marvelcheng <295567172@qq.com>
2022-01-25 09:31:18 +0800

    check in

---
+++ sword/dynamic.go
@@ -0,0 +1,137 @@
+package sword
+
+/*
+https://leetcode-cn.com/problems/min-cost-climbing-stairs/
+
+746. 使用最小花费爬楼梯
+给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+请你计算并返回达到楼梯顶部的最低花费。
+
+示例 1：
+
+输入：cost = [10,15,20]
+输出：15
+解释：你将从下标为 1 的台阶开始。
+- 支付 15 ，向上爬两个台阶，到达楼梯顶部。
+总花费为 15 。
+
+示例 2：
+输入：cost = [1,100,1,1,1,100,1,1,100,1]
+输出：6
+解释：你将从下标为 0 的台阶开始。
+- 支付 1 ，向上爬两个台阶，到达下标为 2 的台阶。
+- 支付 1 ，向上爬两个台阶，到达下标为 4 的台阶。
+- 支付 1 ，向上爬两个台阶，到达下标为 6 的台阶。
+- 支付 1 ，向上爬一个台阶，到达下标为 7 的台阶。
+- 支付 1 ，向上爬两个台阶，到达下标为 9 的台阶。
+- 支付 1 ，向上爬一个台阶，到达楼梯顶部。
+总花费为 6 。
+
+提示：
+
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999
+*/
+func minCostClimbingStairs(cost []int) int {
+	dp := [2]int{cost[0], cost[1]}
+	for i := 2; i < len(cost); i++ {
+		dp[i%2] = min(dp[0], dp[1]) + cost[i]
+	}
+
+	return min(dp[0], dp[1])
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+/*
+https://leetcode-cn.com/problems/house-robber/
+
+198. 打家劫舍
+你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+
+示例 1：
+输入：[1,2,3,1]
+输出：4
+解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+     偷窃到的最高金额 = 1 + 3 = 4 。
+
+示例 2：
+输入：[2,7,9,3,1]
+输出：12
+解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+
+提示：
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 400
+*/
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	dp := [2]int{nums[0], max(nums[0], nums[1])}
+	for i := 2; i < len(nums); i++ {
+		dp[i%2] = max(nums[i]+dp[i%2], dp[(i-1)%2])
+	}
+	return max(dp[0], dp[1])
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+/*
+https://leetcode-cn.com/problems/partition-equal-subset-sum/
+
+416. 分割等和子集
+给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+
+示例 1：
+
+输入：nums = [1,5,11,5]
+输出：true
+解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+示例 2：
+
+输入：nums = [1,2,3,5]
+输出：false
+解释：数组不能分割成两个元素和相等的子集。
+
+提示：
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+*/
+func canPartition(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+
+	if sum%2 == 1 {
+		return false
+	}
+	t := sum / 2
+
+	dp := make([]bool, t+1)
+	dp[0] = true
+	for i := 1; i < len(nums); i++ {
+		for j := t; j >= 1; j-- {
+			dp[j] = dp[j] || (j-nums[i-1] >= 0 && dp[j-nums[i-1]])
+		}
+	}
+
+	return dp[t] || (t-nums[len(nums)-1] >= 0 && dp[t-nums[len(nums)-1]])
+}
---
+++ sword/dynamic_test.go
@@ -0,0 +1,61 @@
+package sword
+
+import "testing"
+
+func Test_rob(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			args: args{[]int{1, 2, 3, 1}},
+			want: 4,
+		},
+		{
+			args: args{[]int{2, 7, 9, 3, 1}},
+			want: 12,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rob(tt.args.nums); got != tt.want {
+				t.Errorf("rob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_canPartition(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			args: args{[]int{1, 5, 11, 5}},
+			want: true,
+		},
+		{
+			args: args{[]int{1, 2, 3, 5}},
+			want: false,
+		},
+		{
+			args: args{[]int{1, 2, 5}},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canPartition(tt.args.nums); got != tt.want {
+				t.Errorf("canPartition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
