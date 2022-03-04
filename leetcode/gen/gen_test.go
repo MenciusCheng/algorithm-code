ebd80d3bd1228a0ba5a3dd40779aadde923b8584
marvelcheng <295567172@qq.com>
2022-03-04 10:04:18 +0800

    check in

--- leetcode/gen/gen_test.go
+++ leetcode/gen/gen_test.go
@@ -4,48 +4,38 @@ import "testing"
 
 func TestGen(t *testing.T) {
 	desc := `
-1380. 矩阵中的幸运数
-给你一个 m * n 的矩阵，矩阵中的数字 各不相同 。请你按 任意 顺序返回矩阵中的所有幸运数。
+258. 各位相加
+给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。返回这个结果。
 
-幸运数是指矩阵中满足同时下列两个条件的元素：
-在同一行的所有元素中最小
-在同一列的所有元素中最大
+示例 1:
 
-示例 1：
+输入: num = 38
+输出: 2 
+解释: 各位相加的过程为：
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2
+由于 2 是一位数，所以返回 2。
+示例 1:
 
-输入：matrix = [[3,7,8],[9,11,13],[15,16,17]]
-输出：[15]
-解释：15 是唯一的幸运数，因为它是其所在行中的最小值，也是所在列中的最大值。
-示例 2：
-
-输入：matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
-输出：[12]
-解释：12 是唯一的幸运数，因为它是其所在行中的最小值，也是所在列中的最大值。
-示例 3：
-
-输入：matrix = [[7,8],[1,2]]
-输出：[7]
+输入: num = 0
+输出: 0 
 
 提示：
 
-m == mat.length
-n == mat[i].length
-1 <= n, m <= 50
-1 <= matrix[i][j] <= 10^5
-矩阵中的所有元素都是不同的
+0 <= num <= 2^31 - 1
 `
 
 	url := `
-https://leetcode-cn.com/problems/lucky-numbers-in-a-matrix/
+https://leetcode-cn.com/problems/add-digits/
 `
 
 	cal := `
-func luckyNumbers (matrix [][]int) []int {
+func addDigits(num int) int {
 
 }
 `
 
-	month := "m202202"
+	month := "m202203"
 
 	if err := Gen(desc, url, cal, month); err != nil {
 		t.Errorf("Gen error: %+v", err)
