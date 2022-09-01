4459fcd3396fa1253ed9477f1734691134cf8737
marvelcheng <295567172@qq.com>
2022-09-01 10:33:12 +0800

    check in

--- leetcode/gen/gen_test.go
+++ leetcode/gen/gen_test.go
@@ -7,46 +7,48 @@ import (
 
 func TestGen(t *testing.T) {
 	desc := `
-623. 在二叉树中增加一行
-给定一个二叉树的根 root 和两个整数 val 和 depth ，在给定的深度 depth 处添加一个值为 val 的节点行。
-注意，根节点 root 位于深度 1 。
+1475. 商品折扣后的最终价格
+给你一个数组 prices ，其中 prices[i] 是商店里第 i 件商品的价格。
+商店里正在进行促销活动，如果你要买第 i 件商品，那么你可以得到与 prices[j] 相等的折扣，其中 j 是满足 j > i 且 prices[j] <= prices[i] 的 最小下标 ，如果没有满足条件的 j ，你将没有任何折扣。
+请你返回一个数组，数组中第 i 个元素是折扣后你购买商品 i 最终需要支付的价格。
 
-加法规则如下:
+示例 1：
 
-给定整数 depth，对于深度为 depth - 1 的每个非空树节点 cur ，创建两个值为 val 的树节点作为 cur 的左子树根和右子树根。
-cur 原来的左子树应该是新的左子树根的左子树。
-cur 原来的右子树应该是新的右子树根的右子树。
-如果 depth == 1 意味着 depth - 1 根本没有深度，那么创建一个树节点，值 val 作为整个原始树的新根，而原始树就是新根的左子树。
+输入：prices = [8,4,6,2,3]
+输出：[4,2,4,2,3]
+解释：
+商品 0 的价格为 price[0]=8 ，你将得到 prices[1]=4 的折扣，所以最终价格为 8 - 4 = 4 。
+商品 1 的价格为 price[1]=4 ，你将得到 prices[3]=2 的折扣，所以最终价格为 4 - 2 = 2 。
+商品 2 的价格为 price[2]=6 ，你将得到 prices[3]=2 的折扣，所以最终价格为 6 - 2 = 4 。
+商品 3 和 4 都没有折扣。
+示例 2：
 
-示例 1:
+输入：prices = [1,2,3,4,5]
+输出：[1,2,3,4,5]
+解释：在这个例子中，所有商品都没有折扣。
+示例 3：
 
-输入: root = [4,2,6,3,1,5], val = 1, depth = 2
-输出: [4,1,1,2,null,null,6,3,1,5]
-示例 2:
+输入：prices = [10,1,1,6]
+输出：[9,0,1,6]
+ 
 
-输入: root = [4,2,null,3,1], val = 1, depth = 3
-输出:  [4,2,null,1,1,3,null,null,1]
+提示：
 
-提示:
-
-节点数在 [1, 10^4] 范围内
-树的深度在 [1, 10^4]范围内
--100 <= Node.val <= 100
--105 <= val <= 10^5
-1 <= depth <= the depth of tree + 1
+1 <= prices.length <= 500
+1 <= prices[i] <= 10^3
 `
 
 	url := `
-https://leetcode.cn/problems/add-one-row-to-tree/
+https://leetcode.cn/problems/final-prices-with-a-special-discount-in-a-shop/
 `
 
 	cal := `
-func addOneRow(root *TreeNode, val int, depth int) *TreeNode {
+func finalPrices(prices []int) []int {
 
 }
 `
 
-	month := "m202208"
+	month := "m202209"
 
 	if err := Gen(desc, url, cal, month); err != nil {
 		t.Errorf("Gen error: %+v", err)
@@ -65,7 +67,10 @@ func TestArrStr(t *testing.T) {
 		{
 			args: args{
 				str: `
-[[10,60],[20,30],[30,30],[60,40]]
+输入：groupSizes = [3,3,3,3,3,1,3]
+输出：[[5],[0,1,2],[3,4,6]]
+输入：groupSizes = [2,1,3,3,3,2]
+输出：[[1],[0,5],[2,3,4]]
 `,
 			},
 		},
