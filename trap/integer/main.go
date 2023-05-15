5382cc42c05ec0eac7a1e243cc9cdc661e143108
MenciusCheng <295567172@qq.com>
2023-05-15 16:32:37 +0800

    浮点数显示

--- trap/integer/main.go
+++ trap/integer/main.go
@@ -3,8 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	fmtFloat64()
+}
+
+func fmtInt() {
 	x := int(1e9 + 7)
 	fmt.Printf("x=%d\n", x)
 	fmt.Printf("?=1000000007\n")
 	fmt.Println(x == 1000000007)
+}
+
+func fmtFloat64() {
+	fmt.Printf("%.2f\n", float64(100.0))
+	fmt.Printf("%.2f\n", float64(100.23))
+	fmt.Printf("%v\n", float64(100.0))
+	fmt.Printf("%v\n", float64(100.23))
 }
